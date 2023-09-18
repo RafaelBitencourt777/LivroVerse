0f7568e966cbd634e98919e9bb3fc348fbd59f10
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-09-17 23:44:15 -0300

    final

--- src/pages/cadastrocliente/App.js
+++ src/pages/cadastrocliente/App.js
@@ -1,7 +1,32 @@
 import './App.scss';
+import React, { useState } from 'react';
 import fundolivros from '../assets/img/livros.png';
+import axios from 'axios';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const handleCadastro = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/cliente/cadastrar', {
+        nm_cliente: nome,
+        ds_email: email,
+        ds_senha: senha,
+      });
+
+      if (response.status === 201) {
+        setMensagem('Cliente cadastrado com sucesso.');
+      } else {
+        setMensagem('Ocorreu um erro ao cadastrar o cliente.');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer a solicitação:', error);
+    }
+  };
+
   return (
     <div className='tudo'>
       <div className='fundo' style={{ backgroundImage: `url(${fundolivros})` }}>
@@ -9,33 +34,41 @@ function App() {
           <h3>Cadastre-se</h3>
           <div className='input'>
             <label htmlFor="nome">Nome Completo</label>
-            <input type="text" id="nome" placeholder="Nome" />
+            <input
+              type="text"
+              id="nome"
+              placeholder="Nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
           </div>
 
           <div className='input'>
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" placeholder="Email" />
-          </div>
-
-          <div className='input'>
-            <label htmlFor="confirmarEmail">Confirme o Email</label>
-            <input type="text" id="confirmarEmail" placeholder="Confirmar Email" />
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
           <div className='input'>
             <label htmlFor="senha">Senha</label>
-            <input type="password" id="senha" placeholder="Senha" />
+            <input
+              type="password"
+              id="senha"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
           </div>
 
-          <div className='input'>
-            <label htmlFor="confirmarSenha">Confirmar Senha</label>
-            <input type="password" id="confirmarSenha" placeholder="Confirmar Senha"/>
-          </div>
-          <button className="button-criar" type="submit">Criar</button>
-        </div>
-        <div className="termos">
-          <input type="checkbox" id="termosCheckbox" name="termos" />
-          <label htmlFor="termosCheckbox">Li e aceito os termos de uso e os termos de privacidade</label>
+          <button className="button-criar" onClick={handleCadastro}>
+            Criar
+          </button>
+          <div className="mensagem">{mensagem}</div>
         </div>
       </div>
     </div>
--- src/pages/logincliente/App.js
+++ src/pages/logincliente/App.js
@@ -1,10 +1,30 @@
 import './App.scss';
+import React, { useState } from 'react';
 import fundolivros from '../assets/img/livros.png';
 import googleLogo from '../assets/img/google-logo.png';
 import facebookLogo from '../assets/img/facebook-logo.png';
 import appleLogo from '../assets/img/apple-logo.png';
+import axios from 'axios';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const login = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/cliente/login', { email, senha });
+
+      if (response.status === 200) {
+        setMensagem('Login bem-sucedido!');
+      } else {
+        setMensagem('Credenciais inválidas. Tente novamente.');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer a solicitação:', error);
+    }
+  };
+
   return (
     <div className='tudo'>
       <div className='fundo' style={{ backgroundImage: `url(${fundolivros})` }}>
@@ -12,14 +32,28 @@ function App() {
           <h3>Login</h3>
           <div className='input'>
             <label htmlFor="email">Email ou telefone</label>
-            <input type="text" id="email" placeholder="Email ou telefone" />
+            <input
+              type="text"
+              id="email"
+              placeholder="Email ou telefone"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className='input'>
             <label htmlFor="senha">Senha</label>
-            <input type="password" id="senha" placeholder="Senha" />
-            <a href="/recuperar-senha" className="forgot-password">Esqueci minha senha</a>
+            <input
+              type="password"
+              id="senha"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
           </div>
-          <button className="button-entrar" type="submit">Entrar</button>
+          <button className="button-entrar" onClick={login}>
+            Entrar
+          </button>
+          <div className="mensagem">{mensagem}</div>
           <div className="social-buttons">
             <button className="social-button google">
               <img src={googleLogo} alt="Google Logo" className="social-icon" />
@@ -35,7 +69,6 @@ function App() {
             </button>
           </div>
         </div>
-
       </div>
     </div>
   );
