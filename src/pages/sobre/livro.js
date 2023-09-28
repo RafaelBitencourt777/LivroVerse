f2ebd5464c0f997e0ef8457f9a905f0b46ea1cc5
MurilloGomesDenadai <ra43140080832@acaonsfatima.org.br>
2023-09-28 11:37:59 -0300

    Arrumei e finalmente acabei o Sobre

--- src/pages/sobre/livro.js
+++ src/pages/sobre/livro.js
@@ -7,20 +7,19 @@ import Rodape from '../../components/rodape/index.js';
 export default function Sobre() {
   return (
     <div className='tudo'>
-      <Cabecalho/>
-        <img className='livrocim' src={livrocima} alt='' />
-        <div className='meio'>
-            <h4>A nossa e sua livraria possui o seu gênero de livro favorito, desde romance à aventura.
-            </h4>
+      <Cabecalho />
+      <img className='livrocim' src={livrocima} alt='' />
+      <div className='meio'>
+        <h4>A nossa e sua livraria possui o seu gênero de livro favorito, desde romance à aventura.</h4>
+      </div>
+      <div className='final'>
+        <img className='livroh' src={livroheart} alt='' />
+        <div className='textos'>
+          <p className='titulo'><b> Um site para os Amantes de Literatura</b></p>
+          <p>Diversos tipos de livros físicos, PDF e Kindle. Além de marca-páginas de variados estilos.</p>
         </div>
-        <div className='final'>
-            <img className='livroh' src={livroheart} alt=''/>
-            <div className='textos'>
-            <p className='titulo'><b> Um site para os Amantes de Literatura</b></p>
-            <p>Diversos tipos de livros físico, PDF e Kindle. Além de marca páginas de variados estilos.</p>
-            </div>
-        </div>
-        <Rodape/>
+      </div>
+      <Rodape />
     </div>
-  )
+  );
 }
