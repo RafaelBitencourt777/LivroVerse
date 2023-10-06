b3b59c92a75aaae821a3688e7bcf8991736f50c2
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-10-06 09:19:29 -0300

    cmmitw

--- src/pages/favoritos/index.js
+++ src/pages/favoritos/index.js
@@ -1,12 +1,128 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
 
-export default function favoritos(){
+import Cabecalho from '../../components/cabecalho';
+import voltar from '../../assets/img/setavoltar.png';
+import heart from '../../assets/img/corasao.png';
 
+export default function Favoritos() {
 
-    return(
-        <div>
-            
+    return (
+        <div className='Fav'>
+
+            <Cabecalho/>
+
+            <div className='f-meio'>
+
+                <div className='m-cima'>
+                    <img src={voltar} alt='' />
+                    <h1>Favoritos</h1>
+                </div>
+
+                <div className='quadro'>
+
+                    <div className='f-cards'>
+                        
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                        <div className='f-card'>
+
+                            <img src={heart} alt='' />
+
+                            <div className='livro'></div>
+
+                            <h3>Nome do Livro</h3>
+
+                            <button>Comprar</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
-};
+}
