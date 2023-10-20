07266b8a5a353e86671184fa7f6cb8627d2f1c63
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-10-20 08:50:12 -0300

    tedte

--- src/components/barral/index.js
+++ src/components/barral/index.js
@@ -1,50 +1,47 @@
+import React from 'react';
 import './index.scss';
 
-export default function BarraLateral() {
-
-
-
-
-    return(
-        <div className='BarraLateral'>
-            <div className='cima'>
-                <div className='user'>
-                    <img src='' alt=''/>
-                </div>
-
-                <div className='textos'>
-                    <h2>Nome do user</h2>
-                    <h5>Email do user</h5>
-                    <h3>Sua conta está nível:</h3>
-                    <h2>Nível da conta</h2>
-                </div>
-            </div>
-
-            <div className='baixo'>
-                <div>
-                    <h2>Meus Pedidos</h2>
-                </div>
-                
-                <div>
-                    <h2>Minha Conta</h2>
-                </div>
-                
-                <div>
-                    <h2>Favoritos</h2>
-                </div>
-                
-                <div>
-                    <h2>Historico de navegação</h2>
-                </div>
-                
-                <div>
-                    <h2>Lista de Favoritos</h2>
-                </div>
-
-                <div>
-                    <h2>Configurações</h2>
-                </div>
-            </div>
+export default function BarraLateral({ fotoPerfil, nome, email, nivelConta }) {
+  return (
+    <div className='BarraLateral'>
+      <div className='cima'>
+        <div className='user'>
+          <img src={fotoPerfil} alt='Foto de Perfil' />
         </div>
-    )
-} 
+
+        <div className='textos'>
+          <h2>{nome}</h2>
+          <h5>{email}</h5>
+          <h3>Sua conta está no nível:</h3>
+          <h2>{nivelConta}</h2>
+        </div>
+      </div>
+
+      <div className='baixo'>
+        <div>
+          <h2>Meus Pedidos</h2>
+        </div>
+
+        <div>
+          <h2>Minha Conta</h2>
+        </div>
+
+        <div>
+          <h2>Favoritos</h2>
+        </div>
+
+        <div>
+          <h2>Historico de navegação</h2>
+        </div>
+
+        <div>
+          <h2>Lista de Favoritos</h2>
+        </div>
+
+        <div>
+          <h2>Configurações</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
