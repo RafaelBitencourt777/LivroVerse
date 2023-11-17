090f5cbf407529cf61dfcaa27dd2d3bed8a15d9d
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-11-17 01:11:03 -0300

    aaaaaaaaaaaaaaaaaaavdv

--- src/pages/cadastrarProd/index.js
+++ src/pages/cadastrarProd/index.js
@@ -1,59 +1,102 @@
-import './index.scss';
-
+import React, { useState } from 'react';
 import user from '../../assets/img/useradm.png';
 import up from '../../assets/img/cadastrar-img.png';
+import axios from 'axios';
 
 export default function CadastrarProd() {
+  const [produto, setProduto] = useState({
+    id_categoria: '', 
+    id_avaliacao: '', 
+    nm_produto: '',
+    vl_preco_fis: '',
+    ds_desc: '',
+    vl_preco_pdf: '',
+    vl_preco_kindle: '',
+    vl_preco_promocional: '',
+    bt_destaque: '',
+    bt_promocao: '',
+    bt_disponivel: '',
+    qtd_estoque: '',
+    bl_favorito: '',
+    imagem: null,
+  });
 
-    return(
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProduto({
+      ...produto,
+      [name]: value,
+    });
+  };
 
-        <div className='CadastrarProd'>
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setProduto({
+      ...produto,
+      imagem: file,
+    });
+  };
 
-            <img className='cad-user' src={user} alt='' />
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-            <div className='cadp-quad'>
+    try {
+      const formData = new FormData();
+      for (const key in produto) {
+        formData.append(key, produto[key]);
+      }
 
-                <h1>Adicione um novo produto</h1>
+      const response = await axios.post('http://129.148.42.252:5018/produto/cadastrar', formData);
 
-                <div className='inps1'>
+      if (response.status === 201) {
+        console.log('Produto cadastrado com sucesso!');
+      } else {
+        console.error('Erro ao cadastrar o produto.');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer a solicitação:', error);
+    }
+  };
 
-                    <input className='Cad-name' type='text' placeholder='Nome do Produto'/>
+  return (
+    <div className='CadastrarProd'>
+      <img className='cad-user' src={user} alt='' />
+      <div className='cadp-quad'>
+        <h1>Adicione um novo produto</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='inps1'>
+            <input
+              className='Cad-name'
+              type='text'
+              placeholder='Nome do Produto'
+              name='nm_produto'
+              value={produto.nm_produto}
+              onChange={handleInputChange}
+            />
+            <input
+              className='Cad-quant'
+              type='number'
+              placeholder='Quantidade'
+              name='qtd_estoque'
+              value={produto.qtd_estoque}
+              onChange={handleInputChange}
+            />
+          </div>
 
-                    <input className='Cad-quant' type='number' placeholder='Quantidade'/>
 
-                </div>
+          <div className='inp-pe'>
+            <input
+              className='Cad-img'
+              type='file'
+              accept='image/*'
+              onChange={handleFileChange}
+            />
+            <img src={up} alt='' />
+          </div>
 
-                <div className='inps2'>
-
-                    <input className='Cad-cod' type='number' placeholder='Código'/>
-
-                    <input className='Cad-est' type='number' placeholder='Estoque'/>
-
-                    <input className='Cad-preco' type='number' placeholder='Preço'/>
-
-                </div>
-
-                <div className='inps3'>
-
-                    <input className='Cad-desc' type='text' placeholder='Descrição do Produto'/>
-
-                    <div className='inp-pe'>
-
-                        <input className='Cad-gen' type='text' placeholder='Gênero do Produto'/>
-
-                        <input className='Cad-img' placeholder='Imagem do Produto'/>
-                        
-                        <img src={up} alt=''/>
-                        
-                    </div>
-
-                </div>
-
-                <button>Cadastrar Produto</button>
-
-            </div>
-
-        </div>
-
-    )
+          <button type='submit'>Cadastrar Produto</button>
+        </form>
+      </div>
+    </div>
+  );
 }
