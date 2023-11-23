601039c32802a30d32492ce8fe7b861a1791020c
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-11-23 08:24:03 -0300

    a

--- API-main/src/controller/admController.js
+++ API-main/src/controller/admController.js
@@ -1,13 +1,13 @@
+
 import { Router } from 'express';
-import { realizarLoginAdm ,criarContaAdm ,teste} from '../repository/admRepository.js';
+import { realizarLoginAdm, criarContaAdm, teste } from '../repository/admRepository.js';
 
 const endpoints = Router();
+
 endpoints.post('/adm/login', async (req, resp) => {
   try {
     const { ds_email, ds_senha } = req.body;
-
     const administrador = await realizarLoginAdm(ds_email, ds_senha);
-
     resp.status(200).send(administrador);
   } catch (error) {
     resp.status(401).send({ erro: 'Credenciais de administrador inválidas.' });
@@ -16,10 +16,13 @@ endpoints.post('/adm/login', async (req, resp) => {
 
 endpoints.get('/adm/usuarios', async (req, resp) => {
   try {
-    const usuarios = await(teste);
+    const usuarios = await teste().catch(error => {
+      resp.status(500).send({ erro: 'Ocorreu um erro ao obter a lista de usuários.' + error.message });
+    });
 
+    // Se não houver usuários, retorna 204 (No Content)
     if (usuarios.length === 0) {
-    ;
+      resp.status(204).end();
     } else {
       resp.status(200).json(usuarios);
     }
@@ -29,15 +32,13 @@ endpoints.get('/adm/usuarios', async (req, resp) => {
 });
 
 endpoints.post('/adm/criar-conta', async (req, resp) => {
-    try {
-        const { nm_administrador, ds_email, ds_senha } = req.body;
-
-        const novoAdministrador = await criarContaAdm(nm_administrador, ds_email, ds_senha);
-
-        resp.status(201).send(novoAdministrador);
-    } catch (error) {
-        resp.status(400).send({ erro: error.message });
-    }
+  try {
+    const { nm_administrador, ds_email, ds_senha } = req.body;
+    const novoAdministrador = await criarContaAdm(nm_administrador, ds_email, ds_senha);
+    resp.status(201).send(novoAdministrador);
+  } catch (error) {
+    resp.status(400).send({ erro: error.message });
+  }
 });
 
 export default endpoints;
