bce7943b367e269036ae8fa78b50a38ddd77bd42
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-11-22 22:11:30 -0300

    Atualizar o index.js
    ajustes

--- src/pages/principalAdm/index.js
+++ src/pages/principalAdm/index.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 
 import user from '../../assets/img/useradm.png';
@@ -8,104 +9,90 @@ import pc from '../../assets/img/produtos-cadastrados.png';
 import relatorios from '../../assets/img/relatorios.png';
 
 export default function PrincAdm() {
+  const [nomeDaConta, setNomeDaConta] = useState('');
 
-    return (
+  useEffect(() => {
+    const fazerLogin = async () => {
+      try {
+        const response = await fetch('/adm/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            ds_email: 'seuEmail',
+            ds_senha: 'suaSenha',
+          }),
+        });
 
-        <div className='PrincAdm'>
+        const { nome } = await response.json();
 
-            <div className='adm-esquerda'>
+        setNomeDaConta(nome);
+      } catch (error) {
+        console.error('Erro ao fazer login:', error);
+      }
+    };
 
-                <div className='adm-ecima'>
+    fazerLogin();
+  }, []);
 
-                    <img src={user} alt='' />
-
-                    <p>Olá Sr.</p>
-
-                </div>
-
-                <div className='adm-ebaixo'>
-
-                    <button>Pedidos</button>
-
-                    <button>Produtos</button>
-
-                    <a href='/cadastrar-produto'>
-
-                        <button>Adicionar Produtos</button>
-                    
-                    </a>
-
-                    
-                    <a href='/usuarios'>
-                        <button>Usuários</button>
-                    </a>
-                    
-
-                    <button>Mensagens</button>
-
-                    <button>Relatórios</button>
-
-                    <button>Gerenciar Produtos</button>
-
-                </div>
-
-            </div>
-
-            <div className='adm-direita'>
-
-                <div className='adm-dcima'>
-
-                    <img src={logo} alt='' />
-
-                </div>
-
-                <div className='adm-dbaixo'>
-
-                    <div className='dois-quad'>
-
-                        <div className='adm-quadrado'>
-
-                            <h2>Pedidos</h2>
-
-                            <img src={pedidos} alt='' />
-
-                        </div>
-
-                        <div className='adm-quadrado'>
-
-                            <h2>Produtos Cadastrados</h2>
-
-                            <img src={pc} alt='' />
-
-                        </div>
-
-                    </div>
-
-                    <div className='dois-quad'>
-
-                        <div className='adm-quadrado'>
-
-                            <h2>Gerenciar Produtos</h2>
-
-                            <img src={gp} alt='' />
-
-                        </div>
-
-                        <div className='adm-quadrado'>
-
-                            <h2>Relatórios</h2>
-
-                            <img src={relatorios} alt='' />
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
+  return (
+    <div className='PrincAdm'>
+      <div className='adm-esquerda'>
+        <div className='adm-ecima'>
+          <img src={user} alt='' />
+          <p>Olá Sr. {nomeDaConta}</p>
         </div>
 
-    )
+        <div className='adm-ebaixo'>
+          <button>Pedidos</button>
+          <button>Produtos</button>
+
+          <a href='/cadastrar-produto'>
+            <button>Adicionar Produtos</button>
+          </a>
+
+          <a href='/usuarios'>
+            <button>Usuários</button>
+          </a>
+
+          <button>Mensagens</button>
+          <button>Relatórios</button>
+          <button>Gerenciar Produtos</button>
+        </div>
+      </div>
+
+      <div className='adm-direita'>
+        <div className='adm-dcima'>
+          <img src={logo} alt='' />
+        </div>
+
+        <div className='adm-dbaixo'>
+          <div className='dois-quad'>
+            <div className='adm-quadrado'>
+              <h2>Pedidos</h2>
+              <img src={pedidos} alt='' />
+            </div>
+
+            <div className='adm-quadrado'>
+              <h2>Produtos Cadastrados</h2>
+              <img src={pc} alt='' />
+            </div>
+          </div>
+
+          <div className='dois-quad'>
+            <div className='adm-quadrado'>
+              <h2>Gerenciar Produtos</h2>
+              <img src={gp} alt='' />
+            </div>
+
+            <div className='adm-quadrado'>
+              <h2>Relatórios</h2>
+              <img src={relatorios} alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
