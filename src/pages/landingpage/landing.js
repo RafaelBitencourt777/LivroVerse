4175e1af4ca597c6951a9fc56fc15bf6e3ef5984
Psych-ait <135169772+Psych-ait@users.noreply.github.com>
2023-11-24 01:48:43 -0300

    a

--- src/pages/landingpage/landing.js
+++ src/pages/landingpage/landing.js
@@ -1,53 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './landing.scss';
-
 import Cabecalho from '../../components/cabecalho/index.js';
 import Rodape from '../../components/rodape/index.js';
-import lesq from '../../assets/img/lesq.png';
-import ldir from '../../assets/img/ldir.png';
-import estrelas from '../../assets/img/estrelas5.png';
-import axios from 'axios';
 import oftesq from '../../assets/img/oftesq.png';
 import oftdir from '../../assets/img/oftdir.png';
+import estrelas from '../../assets/img/estrelas5.png';
+import lesq from '../../assets/img/lesq.png';
+import ldir from '../../assets/img/ldir.png';
 import ad1 from '../../assets/img/1banneroferta.png';
 import ad2 from '../../assets/img/2banneroferta.png';
 import ad3 from '../../assets/img/3banneroferta.png';
 
-
 function App() {
-  const [livros, setLivros] = useState([]);
   const [currentAd, setCurrentAd] = useState(1);
-
-  useEffect(() => {
-    axios.get('http://129.148.42.252:5018/landing-page')
-      .then(response => setLivros(response.data))
-      .catch(error => console.error('Erro ao obter os livros:', error));
-  }, []);
-
-  const renderizarLivros = () => {
-    return livros.map(livro => (
-      <div className="card" key={livro.id}>
-        <a href={`/previsualizacao/${livro.id}`}>
-          <div className="imagem">
-            <img src={livro.imagem} alt={`Capa de ${livro.nome}`} />
-          </div>
-          <img src={estrelas} alt={`Avaliação: ${livro.avaliacao}`} />
-          <h4>{livro.nome}</h4>
-          <h2>{`R$ ${livro.preco.toFixed(2)}`}</h2>
-        </a>
-      </div>
-    ));
-  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentAd((prevAd) => (prevAd === 3 ? 1 : prevAd + 1));
-    }, 13000);
+    }, 5000); // Troca a cada 5 segundos, você pode ajustar o intervalo conforme necessário
+
     return () => clearInterval(interval);
   }, []);
 
   const handleArrowClick = (direction) => {
-    clearInterval();
+    clearInterval(); // Limpa o intervalo ao clicar nas setas
     if (direction === 'left') {
       setCurrentAd(currentAd === 1 ? 3 : currentAd - 1);
     } else {
@@ -56,7 +32,7 @@ function App() {
   };
 
   const handleIndicatorClick = (indicator) => {
-    clearInterval(); 
+    clearInterval(); // Limpa o intervalo ao clicar nas bolinhas
     setCurrentAd(indicator);
   };
 
@@ -89,14 +65,90 @@ function App() {
 
           <div className="Ofertasp">
             <div className="Ofertap">
-              <h2 className='chegapfv'>Ir até "Livros em Oferta da Black Week"</h2>
-              <img src="" alt="" />
+              <img src="" alt=""/>
             </div>
 
             <div className="Ofertap">
-              <h2 className='chegapfv'>Ir para a recomendação do dia</h2>
-              <img src="" alt="" />
+              <img src="" alt=""/>
             </div>
+          </div>
+        </div>
+
+        <div className="MaisVen">
+          <h1>LivroVerse Black Week: Até 80% de desconto em vários livros</h1>
+
+          <div className="cards">
+            <img className="lesq" src={lesq} alt="" />
+
+            <div className="card">
+
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>Harry Potter e a Pedra Filosofal</h4>
+
+              <h2>R$ 39,90</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>Cem Anos de Solidão</h4>
+
+              <h2>R$ 45,99</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>1984</h4>
+
+              <h2>R$ 32,50</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>O Grande Gatsby</h4>
+
+              <h2>R$ 28,75</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>O Senhor dos Anéis</h4>
+
+              <h2>R$ 55,00</h2>
+              </a>
+            </div>
+
+            <div className="card">
+
+            </div>
+
+            <img className="ldir" src={ldir} alt=""/>
           </div>
         </div>
 
@@ -106,101 +158,83 @@ function App() {
           <div className="cards">
             <img className="lesq" src={lesq} alt="" />
 
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
+            <div className="card">
 
-            <div className="card"></div>
+            </div>
 
-            <img className="ldir" src={ldir} alt="" />
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>NDom Quixote</h4>
+
+              <h2>R$ 34,25</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>O Pequeno Príncipe</h4>
+
+              <h2>R$ 25,00</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>A Menina que Roubava Livros</h4>
+
+              <h2>R$ 33,45</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>A Culpa é das Estrelas</h4>
+
+              <h2>R$ 36,10</h2>
+              </a>
+            </div>
+
+            <div className="card">
+              <a href='/previsualizaçao'>
+              <div className="imagem"></div>
+
+              <img src={estrelas} alt=""/>
+
+              <h4>O Código Da Vinci</h4>
+
+              <h2>R$ 34,50</h2>
+              </a>
+            </div>
+
+            <div className="card">
+
+            </div>
+
+            <img className="ldir" src={ldir} alt=""/>
           </div>
         </div>
 
-        <div className="MaisVen">
-          <h1>Livros em desconto para nossa Black Week</h1>
-
-          <div className="cards">
-            <img className="lesq" src={lesq} alt="" />
-
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
-
-            <div className="card"></div>
-
-            <img className="ldir" src={ldir} alt="" />
-          </div>
-        </div>
-
-        <div className="MaisVen">
-          <h1>Boas-vindas: Temos ofertas para vocês</h1>
-
-          <div className="cards">
-            <img className="lesq" src={lesq} alt="" />
-
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
-
-            <div className="card"></div>
-
-            <img className="ldir" src={ldir} alt="" />
-          </div>
-        </div>
-
-        <div className="MaisVen">
-          <h1>Livros para te ajudar a refletir</h1>
-
-          <div className="cards">
-            <img className="lesq" src={lesq} alt="" />
-
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
-
-            <div className="card"></div>
-
-            <img className="ldir" src={ldir} alt="" />
-          </div>
-        </div>
-
-        <div className="MaisVen">
-          <h1>Livors de terror</h1>
-
-          <div className="cards">
-            <img className="lesq" src={lesq} alt="" />
-
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
-
-            <div className="card"></div>
-
-            <img className="ldir" src={ldir} alt="" />
-          </div>
-        </div>
-
-        <div className="MaisVen">
-          <h1>Livros de Romance</h1>
-
-          <div className="cards">
-            <img className="lesq" src={lesq} alt="" />
-
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
-
-            <div className="card"></div>
-
-            <img className="ldir" src={ldir} alt="" />
-          </div>
-        </div>
-
-        <div className="MaisVen">
-          <h1>Livros de Aventura</h1>
-
-          <div className="cards">
-            <img className="lesq" src={lesq} alt="" />
-
-            {Array.isArray(livros) && livros.length > 0 ? renderizarLivros() : null}
-
-            <div className="card"></div>
-
-            <img className="ldir" src={ldir} alt="" />
-          </div>
-        </div>
-
+        
+        
       </div>
 
-      <Rodape />
+      <Rodape/>
     </div>
   );
 }
